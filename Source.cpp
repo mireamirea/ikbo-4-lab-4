eb60eb314e7bc14f1509e22fae25f4530c5fe717
Aglophotis <Atisphatis@gmail.com>
2017-05-22 18:25:13 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,48 +9,48 @@ using std::endl;
 
 int main(int argc,char **argv){
 	int i;
-	const unsigned int num_input = 3; //Количество входных сигналов
-	const unsigned int num_output = 1; //Количество выходных сигналов
-	const unsigned int num_layers = 3; //Количество слоев нейронной сети
-	const unsigned int num_neurons_hidden = 3; //Коилчество нейронов в скрытом слое
-	const float desired_error = (const float) 0.01; //Точность
-	const unsigned int max_epochs = 1000; //Количество циклов прогонки(для обучения сети)
-	fann_type *calc_out; //Выходной сигнал
-	fann_type input[3]; //Входные сигналы
+	const unsigned int num_input = 3; //РљРѕР»РёС‡РµСЃС‚РІРѕ РІС…РѕРґРЅС‹С… СЃРёРіРЅР°Р»РѕРІ
+	const unsigned int num_output = 1; //РљРѕР»РёС‡РµСЃС‚РІРѕ РІС‹С…РѕРґРЅС‹С… СЃРёРіРЅР°Р»РѕРІ
+	const unsigned int num_layers = 3; //РљРѕР»РёС‡РµСЃС‚РІРѕ СЃР»РѕРµРІ РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё
+	const unsigned int num_neurons_hidden = 3; //РљРѕРёР»С‡РµСЃС‚РІРѕ РЅРµР№СЂРѕРЅРѕРІ РІ СЃРєСЂС‹С‚РѕРј СЃР»РѕРµ
+	const float desired_error = (const float) 0.01; //РўРѕС‡РЅРѕСЃС‚СЊ
+	const unsigned int max_epochs = 1000; //РљРѕР»РёС‡РµСЃС‚РІРѕ С†РёРєР»РѕРІ РїСЂРѕРіРѕРЅРєРё(РґР»СЏ РѕР±СѓС‡РµРЅРёСЏ СЃРµС‚Рё)
+	fann_type *calc_out; //Р’С‹С…РѕРґРЅРѕР№ СЃРёРіРЅР°Р»
+	fann_type input[3]; //Р’С…РѕРґРЅС‹Рµ СЃРёРіРЅР°Р»С‹
 
-	// создание нейронной сети с именем ann
+	// СЃРѕР·РґР°РЅРёРµ РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё СЃ РёРјРµРЅРµРј ann
 	struct fann *ann = fann_create_standard(num_layers, num_input,num_neurons_hidden, num_output);
 
 	fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
 	fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
-	//обучение
+	//РѕР±СѓС‡РµРЅРёРµ
 	//fann_train_on_file(ann, "training.txt", max_epochs,10, desired_error);
 
-	//Выгрузка обученной нейронной сети
+	//Р’С‹РіСЂСѓР·РєР° РѕР±СѓС‡РµРЅРЅРѕР№ РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё
 	ann = fann_create_from_file("func.net");
 	
-	//задание значений входным нейронам
+	//Р·Р°РґР°РЅРёРµ Р·РЅР°С‡РµРЅРёР№ РІС…РѕРґРЅС‹Рј РЅРµР№СЂРѕРЅР°Рј
 	for (i = 0; i < 3; i++)
 		if (argv[i + 1][0] == '0')
 			input[i] = 0;
 		else
 			input[i] = 1;
 
-	//Считаем значение выходного сигнала
+	//РЎС‡РёС‚Р°РµРј Р·РЅР°С‡РµРЅРёРµ РІС‹С…РѕРґРЅРѕРіРѕ СЃРёРіРЅР°Р»Р°
 	calc_out = fann_run(ann, input);
 
-	//вывод результатов c выходного нейрона
+	//РІС‹РІРѕРґ СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ c РІС‹С…РѕРґРЅРѕРіРѕ РЅРµР№СЂРѕРЅР°
 	printf("%f\n", calc_out[0]);
 	if (calc_out[0] > 0.5)
 		printf("%d", 1);
 	else
 		printf("%d", 0);
 
-	//сохранение обученной сети в файл
+	//СЃРѕС…СЂР°РЅРµРЅРёРµ РѕР±СѓС‡РµРЅРЅРѕР№ СЃРµС‚Рё РІ С„Р°Р№Р»
 	//fann_save(ann, "func.net");
 	
-	//Удаляем структуру
+	//РЈРґР°Р»СЏРµРј СЃС‚СЂСѓРєС‚СѓСЂСѓ
 	fann_destroy(ann);
 	
 	return 0;
