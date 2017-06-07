0ee5691b1344e6e3fa6091ad3a2ed53d93f3ac74
MarMaxim <maksim.marchenko.1997@mail.ru>
2017-06-07 19:11:52 +0300

    Update Marchenko.c

--- Marchenko.c
+++ Marchenko.c
@@ -3,15 +3,28 @@
 #include <string.h>
 
 int i;
-char ans[30];
-void train(char *, char *);
+char ans[100];
+const unsigned int num_input = 3;
+const unsigned int num_layers = 2;
+const unsigned int num_output = 1;
+const float desired_error = (const float) 0.01;
+const unsigned int max_epochs = 200;
+const unsigned int epochs_between_reports = 1;
+
+void train(char *fromwhere, char *towhere) {
+
+	struct fann *myFann = fann_create_standard(num_layers, num_input, num_output);
+	fann_train_on_file(myFann, fromwhere, max_epochs, epochs_between_reports, desired_error);
+	fann_save(myFann, towhere);
+	fann_destroy(myFann);
+}
 
 int main(int argc, char* argv[])
 {
 	if (argc < 4)
 		return 1;
 	if (fopen("classify.net", "r") == NULL) {
-		printf("can't found train file\nwould you like to train?\n");
+		printf("can't found train file\n");
 		printf("input filename or 'exit'\n");
 		scanf("%s", ans);
 		if (strcmp(ans, "exit") != 0)
@@ -21,18 +34,12 @@ int main(int argc, char* argv[])
 
  	struct fann *myFann = fann_create_from_file("classify.net");
 	float input[3];
-	for (i = 1; i <= 3; i++)
-		input[i-1] = (float) *argv[i]-'0';
+	for (i = 0; i < 3; i++)
+		input[i] = (float) *argv[i+1]-'0';
 
  	float *output = fann_run(myFann, input);
-	printf("%.lf\n", *output);
+	printf("%.2f\n", *output);
  	return 0;
 }
 
-void train(char *fromwhere, char *towhere) {
-	struct fann *myFann = fann_create_standard(2, 3, 1);
-	fann_train_on_file(myFann, fromwhere, 200, 1, 0.01);
-	fann_save(myFann, towhere);
-	printf("finish\n");
-	fann_destroy(myFann);
-}
+
